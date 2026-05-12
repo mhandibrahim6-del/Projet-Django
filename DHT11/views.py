ef33ce884d657f9c2ff37248307ac79d51843388
Brahim Mahandi <mhandibrahim6@gmail.com>
2026-05-12 15:34:15 +0100

    Implement date range filtering in dashboard view
    Add date filtering to dashboard view for DHT11 data.

--- DHT11/views.py
+++ DHT11/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.utils.dateparse import parse_datetime
 from .models import DHT11
 
 def dashboard(request):
     mesure = DHT11.objects.order_by('-date').first()
-    mesures = DHT11.objects.order_by('-date')[:20]
-    return render(request, 'dashboard.html', {'mesure': mesure, 'mesures': mesures})
+
+    date_debut = request.GET.get('date_debut')
+    date_fin   = request.GET.get('date_fin')
+
+    mesures = DHT11.objects.order_by('-date')
+
+    if date_debut:
+        mesures = mesures.filter(date__gte=parse_datetime(date_debut))
+    if date_fin:
+        mesures = mesures.filter(date__lte=parse_datetime(date_fin))
+
+    if not date_debut and not date_fin:
+        mesures = mesures[:100]
+
+    return render(request, 'dashboard.html', {
+        'mesure': mesure,
+        'mesures': mesures,
+        'date_debut': date_debut or '',
+        'date_fin':   date_fin or '',
+    })
